262789f71921747f17dc52dc834431961b653439
duskembayev <d.uskembayev@gmail.com>
2021-03-24 11:11:43 +0200

    Improve release script.

--- build/release.bat
+++ build/release.bat
@@ -4,6 +4,7 @@ set version=%1
 set out_dir=out
 
 call dotnet pack -c release -o %out_dir% -p:version=%version% --include-symbols || goto :finish
+call git tag -a release/v%version% -m "Release v%version%" || goto :finish
 call dotnet nuget push %out_dir%\Enhanced.ComponentModel.%version%.nupkg -s nuget.org || goto :finish
 call dotnet nuget push %out_dir%\Enhanced.ComponentModel.%version%.snupkg -s nuget.org || goto :finish
 
